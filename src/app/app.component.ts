7b0cba801747f9b0c8c769c60266afb0e9601172
unknown <g00414386@atu.ie>
2024-02-26 10:30:36 +0000

    Lab 4 completed - Binding lab in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './listcomponent/listcomponent.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+
+onDblClick() {
+  this.isVisable = !this.isVisable;
+}
+
+onClick() {
+  this.counter += 1;
+throw new Error('Method not implemented.');
+}
   title = 'data-binding-app';
+  counter = 0;
+  isVisable : boolean = false;
 }
